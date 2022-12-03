862b106cf511c304acf1fb1203d4b1d98000d314
Ivan Galban <ivan.galban.smith@gmail.com>
2022-12-03 00:29:44 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+var q_a =
+    [
+        ["Cuándo se construyeron las pirámides del sol y la luna ?", "Entre el año 0 y 200 d.C."],
+        ["Cultura con más desarrollo en el posclásico 950 d.C. - 1521 d.C.", "Azteca/Mexica"],
+        ["En qué etapa prehispánica se empezaron a construir los reinos ?", "Preclásica"],
+        ["En qué etapa de la cultura prehispánica se empezaron a construir los reinos ?", "Preclásica"],
+        ["Periodo en el que más se desarrolló la cultura Olmeca", "Preclásico"],
+        ["El mayor desarrollo de la cultura mesoamericana se dio durante el período", "Clásico"],
+        ["época prehispánica que tuvo más dessarrollo en su cultura", "Pos clásico"],
+        ["Qué fue Tenochtitlán ?", "Capital de los Mexicas"],
+        ["Cuándo se fundó Tenochtitlán ?", "1325"],
+        ["Anho en que cayó México Tenochtitlán", "1521"],
+        ["Edificio o museo que alberga el centro de Tenochtitlán", "Templo Mayor"],
+        ["A qué se le conoce como el hombre de Tepexpan", "Es un esqueleto precolombino"],
+        ["Con qué comercializaban en la época prehispánica ? o moneda de intercambio principal de los pueblos mesoamericanos", "Cacao"],
+        ["La base alimenticia de los pueblos mesoamericanos fue", "El maíz"],
+        ["Cuántas lenguas índigenas existen en México ?", "68"],
+        ["Cuál fue uno de los productos artesanales más finos del México antiguo", "La cerámica anaranjada delgada"],
+        ["Cuál es otro término para referirse a los emperadores mexicas ?", "Tlatoani"],
+        ["Cultura madre", "Olmeca"],
+        ["Cuál es la cultura prehispánica que adoraba a los jaguares ?", "Olmeca"],
+        ["En el periodo preclásico cual fue la tribu más dominante ?", "Olmeca"],
+        ["Cultura de las cabezas colosales o cultura del préclasico medio", "Olmeca"],
+        ["Las mesas de piedra para actos ceremoniales son de la cultura", "Olmeca"],
+        ["Cuál es el centro ceremonial más importante de la cultura Olmeca", "La venta, Tabasco"],
+        ["Dónde se encuenta LA PIRáMIDE más antigua ? o primera pirámide mesoamericana hecha de lodo y tierra", "La venta, Tabasco"],
+        ["Centro ceremonial MáS ANTIGUO de la cultura olmeca", "San Lorenzo, Veracruz"],
+        ["Los centros ceremoniales olmecas más importante fueron", "La venta, Tres Zapotes y San Lorenzo"],
+        ["Que civilización se asentó en Chiapas, Yucatán, Veracruz, Guatemala, Belice y Quintana Roo", "Los Mayas"],
+        ["Los mayas se caracterizaban por ser", "Astrónomos y matemáticos"],
+        ["Qué era la cuenta larga ?", "Un calendario de origen Maya"],
+        ["A quien se le atribuye la creación del cero ?", "A los Mayas"],
+        ["Cuál es la Deidad Maya del comercio, también conocido como Dios del Cacao o Sexta deidad maya más comúnmente representada en los códices ?", "Ek Chuah"],
+        ["Dios Maya de la luna", "Ixchel"],
+        ["Cuál es la pirámide MáS ALTA de México", "Toniná (pertenece a la cultura Maya)"],
+        ["Chichén Itzá fue una poderosa ciudad", "Maya"],
+        ["Cultura que se estableció en la Ciudad de Palenque", "Maya"],
+        ["Otro nombre del tempo de Kukulkan, Chichén Itzá", "El Castillo"],
+        ["Manuscrito en papel amate o cuero con representaciones pictóricas que relataba asuntos históricos y religiosos del México antiguo", "Códice"],
+        ["Nombre de centros ceremoniales o ciudades de la cultura Maya", "Palenque, Yaxchilán, Calakmul, Tikal, Toniná, Chichén Itzá, Copán, Uxmal"],
+        ["Qué significa Teotihuacán ?", "Ciudad de los Dioses"],
+        ["Que fué Teotihuacán", "Fue un centro ceremonial y ciudad"],
+        ["Dónde se encuentran las pirámides del Sol y de la Luna", "Teotihuacán, Estado de México"],
+        ["Dónde se ubicaba la Calzada de los Muertos ?", "En Teotihuacán"],
+        ["Cuáles eran los dos grandes grupos étnicos que habitaban en Teotihuacán ?", "Nahuas y Otomíes"],
+        ["Tres productos que los Teotihuacanos obtenían del maguey", "Pulque, cordajes y fibras textiles"],
+        ["Cuál es la deidad suprema de los Teotihuacanos", "Quetzalcóatl"],
+        ["Deidad del México antiguo conocida como la Serpiente Emplumada", "Quetzalcóatl"],
+        ["Dónde se encontraron vestigios de la cultura teotihuacana", "Tikal"],
+        ["Dónde se desarrolló la civilización zapoteca", "En los valles de Etla, Tlacolula, Zimatlán en el estado de Oaxaca"],
+        ["Qué es el Monte Albán", "Fue la antigua capital de los zapotecos"],
+        ["Edificio público de Monte Albán", "Edificio de los danzantes"],
+        ["A qué se debe el nombre del edificio de los danzantes en Monte Albán", "A que las figuras esculpidas en sus lápidas parecen moverse o contorsionarse"],
+        ["Dónde se asentaron los Toltecas", "Tula de Ayende, estado de Hidalgo"],
+        ["Que significa Tolteca ?", "Habitante de Tula"],
+        ["Qúe significa Tula en Náhuatl", "Juncal, o junto al tular"],
+        ["Nombre del altar que construía una cultura prehispánica con la base decorada con cráneos tallados en piedra y estacas para ensartar cabezas de los sacrificados", "Tzompantli"],
+        ["Ciudad donde se usaba el macabro Tzompantli", "Tula"],
+        ["Tribu originaria de Michoacán", "Purépechas"],
+        ["Los tarascos se destacaron por ser", "Artesanos, destacaron como carpinteros y especialmente la metalurgia; trabajaron fundamentalmente el oro y la plata"],
+        ["Quién fue Chalchiuhtlicue", "Diosa de los lagos y corrientes de agua de los Mexicas"],
+        ["Diosa de la fertilidad Mexica", "Coatlicue"],
+        ["Quién era el dios de la guerra Mexica", "Huitzilopochtli"],
+        ["Deidad azteca relacionada con el sol", "Tonatiuh"],
+        ["Deidad azteca de la lluvia y del relámpago", "Tlaloc"],
+        ["Anfibio representando junto al Dios Xólotl", "Ajolote"],
+        ["Lugar mítico de donde salieron los Mexicas o Aztecas", "Aztlán"],
+        ["Qué significa Aztlán ?", "Garza, lugar de las garzas"],
+        ["Terreno contruido sobre las lagunas del Valle de México que sirvió de base al sistema productivo del altiplano central. En la actualidad, puede encontrarse en Xochimilco", ""],
+        ["Cuáles eran las escuelas donde asistían los indiios prehispánicos e hijos de los nobles mexicas", "Calmecac (nobles), Telpochcali (plebeyos o jóvenes del pueblo)"],
+        ["Por qué actividad fueron conocidos los aztecas ?", "Guerreros, agricultura, comercio, cultos religiosos, sacrificios humanos"],
+        ["Qué significa Cuauhtémoc", "Aguila que desciende, o águila que se posa"],
+        ["Significado de la palabra altepetl con la que los tlatoanis denominaban a sus ciudades", "Cerro de agua"],
+        ["Quiénes integraron la triple alianza", "Tenochtitlán, Texcoco, Tlacopán"],
+        ["Etnia de Guanajato", "Chichimeca Jonaz"],
+        ["Cultura índigena que persiste en el norte del país", "Tarahumara"],
+        ["De qué parte del país son los Tarahumaras", "Chihuahua"],
+        ["Tribu índígena de Durango", "Tepehuanes"],
+        ["Tribu indígena de Coahuila", "Kikapú"],
+        ["Comunidad indígena que habita actualmente en Tabasco", "Mayas-Chontales"]
+    ];
+
+var n = q_a.length;
+var i = 0;
+
+
+function answer() {
+    var text = document.getElementById("card-text");
+    text.innerText = q_a[i][1];
+
+    var card = document.getElementById("card");
+    card.classList.add("bg-warning");
+    card.classList.remove(["bg-info"]);
+}
+
+
+function next() {
+    i = Math.floor((Math.random() * n) + 1) - 1;
+    var text = document.getElementById("card-text");
+    text.innerText = q_a[i][0];
+
+    var card = document.getElementById("card");
+    card.classList.add("bg-info");
+    card.classList.remove(["bg-warning"]);
+}
+
+next()
